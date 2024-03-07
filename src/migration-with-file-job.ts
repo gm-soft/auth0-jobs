7659b64221d56bfdc94be16106b755a88f5c4dc7
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-03-07 20:06:45 +0500

    Adjusted

--- src/migration-with-file-job.ts
+++ src/migration-with-file-job.ts
@@ -23,19 +23,27 @@ const migrateWithFileJob = async function() {
         console.log(`${logPrefix}. User ${user.email} has no app metadata`);
         skippedDueToError++;
   
-        await new Promise(f => setTimeout(f, 500));
+        await new Promise(f => setTimeout(f, 300));
         continue;
       }
   
-      if (user.app_metadata.user_type != null) {
+      const appMetadataUserType = user.app_metadata.user_type;
+
+      if (appMetadataUserType != null && !user.email.startsWith('scanner+')) {
         console.log(`${logPrefix}. ${user.email} already has app metadata UserType: ${user.app_metadata.user_type}`);
         skippedDueToAdjusted++;
   
-        await new Promise(f => setTimeout(f, 500));
+        await new Promise(f => setTimeout(f, 300));
         continue;
       }
   
-      const userType = user.user_metadata?.user_type || 'Dashboard';
+      let userType = user.user_metadata?.user_type || 'Dashboard';
+      if (user.email.startsWith('scanner+')) {
+        userType = 'ScannerDevice';
+      }
+
+      console.log(`${logPrefix}. Adjusting user ${user.email} to UserType: ${userType}`);
+
       user.app_metadata = user.app_metadata || {};
       user.app_metadata.user_type = userType;
 
@@ -45,7 +53,7 @@ const migrateWithFileJob = async function() {
         user.app_metadata.permissions = '';
       }
   
-      await new Promise(f => setTimeout(f, 1_000));
+      await new Promise(f => setTimeout(f, 300));
   
       try {
         const updateResponse = await getApiRequest(
@@ -56,7 +64,7 @@ const migrateWithFileJob = async function() {
           },
           accessToken);
   
-        console.log(`${logPrefix}. User ${user.email} adjusted`, updateResponse);
+        console.log(`${logPrefix}. User ${user.email} adjusted`, updateResponse.app_metadata);
         adjusted++;
       } catch (e) {
         console.log(`${logPrefix}. Error adjusting user ${user.email}: ${e}`);
